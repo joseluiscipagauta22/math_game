0b997f5657fbeea25f80e16f2cf03b2c9ca2ebef
Jose Cipagauta <jose.cipagauta52@gmail.com>
2025-07-04 21:42:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'math_game',
+  webDir: 'www'
+};
+
+export default config;
